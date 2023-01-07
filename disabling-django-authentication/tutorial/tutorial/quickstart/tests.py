903bac79f39091f1797418652514009decfa3c3b
Ifrim Silviu <silviu.redent@gmail.com>
2023-01-07 15:32:44 +0200

    Add django article source code.

---
+++ disabling-django-authentication/tutorial/tutorial/quickstart/tests.py
@@ -0,0 +1,14 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+class UserApiTests(APITestCase):
+    base_url = '/users/'
+    
+    def test_get_users(self):
+        status_code = self.client.get(self.base_url).status_code
+        self.assertEqual(status_code, status.HTTP_200_OK)
+
+    def test_users_count(self):
+        users_list = self.client.get(self.base_url).data
+        self.assertEqual(len(users_list), 0)
---
+++ disabling-django-authentication/tutorial/tutorial/quickstart/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+from django.conf import settings
+
+
+class AuthenticationMixin:
+    permission_classes = [
+        permissions.IsAuthenticated if settings.ENABLE_AUTHENTICATION else permissions.AllowAny]
+
+
+class UserViewSet(AuthenticationMixin, viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(AuthenticationMixin, viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
